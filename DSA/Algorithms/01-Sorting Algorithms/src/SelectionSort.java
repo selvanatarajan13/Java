217889500fb864f8f2d2e391b20db44181d7ab6d
selvanatarajan13 <selvanatarajan13@gmail.com>
2025-01-23 16:07:27 +0530

    modified

--- DSA/Algorithms/01-Sorting Algorithms/src/SelectionSort.java
+++ DSA/Algorithms/01-Sorting Algorithms/src/SelectionSort.java
@@ -1,2 +1,18 @@
 public class SelectionSort {
+    public static void main(String[] args) {
+        System.out.println("Bubble Sort");
+        System.out.println("Also, Find the min and max value in the given array");
+
+        int[] array = {70,50,80,20,70,50,30,44,20};
+        int len = array.length;
+        sortArray(array, len);
+    }
+
+    public static void sortArray(int[] array, int n) {
+
+        for (int i = 0; i < n; i++) {
+            
+            if ()
+        }
+    }
 }
